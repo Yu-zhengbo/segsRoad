4e3561dad2a763c80a9140abd7ebaabc415469d7
yuzhengbo <yuzhengbocd@gmail>
2025-05-17 15:26:04 +0800

    初步完成segroad3

---
+++ configs/vmamba/chn6_deep_vmamba_base.py
@@ -0,0 +1,69 @@
+_base_ = [
+    '../_base_/models/upernet_swin.py', '../_base_/datasets/chn6.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        type='Backbone_VSSM',
+        out_indices=(0, 1, 2, 3),
+        pretrained="weights/upernet_vssm_4xb4-160k_ade20k-512x512_base_iter_160000.pth",
+        # copied from classification/configs/vssm/vssm_base_224.yaml
+        dims=128,
+        depths=(2, 2, 15, 2),
+        ssm_d_state=1,
+        ssm_dt_rank="auto",
+        ssm_ratio=2.0,
+        ssm_conv=3,
+        ssm_conv_bias=False,
+        forward_type="v05_noz", # v3_noz,
+        mlp_ratio=4.0,
+        downsample_version="v3",
+        patchembed_version="v2",
+        drop_path_rate=0.6,
+        norm_layer="ln2d",
+        
+    ),
+    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=2),
+    auxiliary_head=dict(in_channels=512, num_classes=2))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=80000,
+        by_epoch=False,
+    )
+]
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+train_dataloader = dict(batch_size=6, num_workers=6)
+val_dataloader = dict(batch_size=1)
+test_dataloader = val_dataloader
